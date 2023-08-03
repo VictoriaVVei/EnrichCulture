c96ddd627ab82d760e10a62cab156f18f9df34bf
Victoria Wei <junjiew@uw.edu>
2023-08-03 01:46:17 -0700

    add qa

---
+++ src/src/pages/Qa.js
@@ -0,0 +1,20 @@
+import React from 'react'; //import React Component
+import { Nav } from '../components/Nav';
+
+export function Qa() {
+
+    return (
+        <div id="Qa">
+            <Nav />
+            <div className='Qa'>
+                <div className='welcomeBoard'></div>
+                <div style={{margin:"40px auto", width:"80%", maxWidth:"800px"}}>
+                    <p style={{fontSize:"larger", fontWeight:"bolder"}}>Q: Why I load so slowly</p>
+                    <p>A: This website applied a lot of js feature, which is dependent on computer's
+                        CPU and wifi connection.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
