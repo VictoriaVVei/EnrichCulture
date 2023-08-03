eafaca5d79153fb3a94d20c98c0627dcb784617c
Victoria Wei <junjiew@uw.edu>
2023-08-03 14:00:01 -0700

    add error page

---
+++ src/src/pages/Error.js
@@ -0,0 +1,15 @@
+import React from 'react'; //import React Component
+
+export function Error() {
+
+    return (
+        <div id="Error">
+            <div className='Error'>
+                <div style={{ textAlign: "center" }}>
+                    <h1 style={{ fontSize: "60px" }}>404</h1>
+                    <p>This is not a right path for Enrich Culture website</p>
+                </div>
+            </div>
+        </div>
+    );
+}
