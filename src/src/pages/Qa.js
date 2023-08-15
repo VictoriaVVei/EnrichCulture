3fced137116dbf12ec9e156573bc8d812cbbd835
siyuanji1 <siyuanji@uw.edu>
2023-08-15 10:11:26 -0700

    textChange

--- src/src/pages/Qa.js
+++ src/src/pages/Qa.js
@@ -9,7 +9,7 @@ export function Qa() {
             <div className='Qa'>
                 <div className='welcomeBoard'></div>
                 <div style={{margin:"40px auto", width:"80%", maxWidth:"800px"}}>
-                    <p style={{fontSize:"larger", fontWeight:"bolder"}}>Q: Why I load so slowly</p>
+                    <p style={{fontSize:"larger", fontWeight:"bolder"}}>Q: Why is the page taking a long time to load?</p>
                     <p>A: This website applied a lot of js feature and firebase reading, which is dependent on computer's
                         CPU and wifi connection.
                     </p>
