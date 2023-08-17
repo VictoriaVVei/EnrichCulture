9c356af4ca73ea7e5fd38cfdb95f3c2c51e1aeb4
siyuanji1 <siyuanji@uw.edu>
2023-08-16 22:10:52 -0700

    addFooter

---
+++ src/src/components/Footer.js
@@ -0,0 +1,12 @@
+export function Footer() {
+    return (
+        <div style={{width: '100%', textAlign: 'center', background: 'white'}}>
+            <h1 aria-label="Contact us">Contact Enrich Culture</h1>
+            <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <span className="material-icons" style={{ marginRight: '8px' }}>email</span> 
+                enrichculture@gmail.com
+            </p>
+            <p>&copy; Enrich Culture 2023</p>
+        </div>
+    );
+}
