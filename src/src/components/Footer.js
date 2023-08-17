1857f3865b104f9a99c42958f510c8a4e04115b1
siyuanji1 <siyuanji@uw.edu>
2023-08-17 00:35:51 -0700

    cssFooter

--- src/src/components/Footer.js
+++ src/src/components/Footer.js
@@ -1,8 +1,8 @@
 export function Footer() {
     return (
-        <div style={{width: '100%', textAlign: 'center', background: 'white'}}>
+        <div style={{width: '100%', textAlign: 'center', background: 'white', height:'225px'}}>
             <h1 aria-label="Contact us">Contact Enrich Culture</h1>
-            <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height:'80px'}}>
                 <span className="material-icons" style={{ marginRight: '8px' }}>email</span> 
                 enrichculture@gmail.com
             </p>
